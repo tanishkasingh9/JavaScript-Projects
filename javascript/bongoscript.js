b5c7d6cd516e39542760987d67a59e6f5cf6ec08
Tanishka Singh <tanishka996@gmail.com>
2021-02-03 21:22:11 -0700

    Add files via upload

---
+++ javascript/bongoscript.js
@@ -0,0 +1,13 @@
+window.onclick = function(e) { 
+
+    if (document.getElementById("lup").src == "1.png") 
+    {
+        document.getElementById("lup").src = "2.png";
+    }
+    else 
+    {
+        document.getElementById("lup").src = "1.png";
+    }
+
+
+};
